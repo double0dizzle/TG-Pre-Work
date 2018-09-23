0c1a92d4400061c50f3e28df0c9128f3d44338d1
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-23 00:31:03 -0600

    Kelvin Weather Exercise

---
+++ JavaScript Modules/app34.js
@@ -0,0 +1,17 @@
+//this is the temperature in Kelvin
+var kelvin = 293;
+//this variable represents the temp in celcius
+var celsius = kelvin - 273;
+//this represents the temp in fahrenheit
+var fahrenheit = celsius*(9/5) + 32
+//this eliminates any decimals in the temperature
+var nonDecimalfahrenheit = Math.floor(celsius*(9/5) + 32)
+
+console.log("The temperature is " + nonDecimalfahrenheit + " degrees Fahrenheit.")
+
+
+//this program can allow forecasts to be made in Celcius and Fahrenheit
+
+Newton = celsius *(33/100)
+NewtonND = Math.floor(Newton)
+console.log(NewtonND)
