7ffc61f068da46b349332694acd241d55ad7eaf3
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-01 10:44:13 -0600

    the .filter method

---
+++ JavaScript Modules/app71.js
@@ -0,0 +1,12 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(randomNumbers =>{
+  return randomNumbers < 250
+});
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+const longFavoriteWords = favoriteWords.filter(longFavoriteWords => {
+  return longFavoriteWords.length > 7;
+});
